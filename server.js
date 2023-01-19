db6e1b9b18436f6ac8f861b6cb4bfac88e09ff00
ikarkam76 <ikarkam76@gmail.com>
2023-01-19 20:22:02 +0200

    change port in back

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const PORT = process.env.PORT || 8080
 app.use(express.json());
 
 
@@ -22,9 +23,9 @@ io.on("connection", (socket) => {
     })
 });
 
-server.listen(8080, (err) => {
+server.listen(PORT, (err) => {
   if (err) {
     throw Error(err);
   }
-  console.log("Example app listening on port 8080!");
+  console.log(`App listening on port ${PORT}`);
 });
