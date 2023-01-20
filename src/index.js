f2e437e489035793c387534fa27b86665151ce66
ikarkam76 <ikarkam76@gmail.com>
2023-01-20 13:35:18 +0200

    back socketprovider to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SocketContext, socket } from "./socket";
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SocketContext.Provider value={socket}>
+  <>
     <App />
-  </SocketContext.Provider>
+  </>
 );
 
