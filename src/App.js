b7c4965a641f83c9343535d29dd0731dc380663d
ikarkam76 <ikarkam76@gmail.com>
2023-01-20 12:10:59 +0200

    change index js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { SocketContext, socket } from "./socket";
 import "./App.css";
 import { Comments } from "./components/Comments";
 
 function App() {
   return (
-    <SocketContext.Provider value={socket}>
+    <>
       <Comments />
-    </SocketContext.Provider>
+    </>
   );
 }
 
