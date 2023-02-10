d6f6821427cf9565a87f29576485c2a0d7092635
ikarkam76 <ikarkam76@gmail.com>
2023-02-10 13:04:20 +0200

    change again

--- src/components/comments/CommentsList.js
+++ src/components/comments/CommentsList.js
@@ -127,7 +127,7 @@ export const CommentsList = () => {
       </Modal>
 
       <List>
-        {comments.map((item, i) => {
+        {comments && (comments.map((item, i) => {
           const { id, user_name, time, comment } = item;
           return (
             <ListItem key={id}>
@@ -168,7 +168,7 @@ export const CommentsList = () => {
               </Accordion>
             </ListItem>
           );
-        })}
+        }))}
       </List>
     </Box>
   );
